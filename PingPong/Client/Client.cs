f7d8f1285f93b3d1372b50920efab4d858fda804
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-02 15:05:45 +0300

    Started implementing the client

---
+++ PingPong/Client/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PingPongClient
+{
+    public class Client
+    {
+        private string _ip;
+        private int _port;
+
+        public Client()
+        {
+
+        }
+
+        public void DefineIpPort(string userInput)
+        {
+            _ip = userInput.Split(':')[0];
+            _port = int.Parse(userInput.Split(':')[1]);
+        }
+
+        public void ConnectToServer()
+        {
+            IPAddress ipa = IPAddress.Parse(_ip);
+
+            IPEndPoint ipe = new IPEndPoint(ipa, _port);
+
+            Socket client = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            client.Connect(ipe);
+        }
+    }
+}
